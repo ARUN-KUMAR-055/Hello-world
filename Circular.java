6713af8c6671f10135171f6c402488cb4d69bbb7
ARUN-KUMAR-055 <34934385+ARUN-KUMAR-055@users.noreply.github.com>
2018-03-07 15:01:11 +0530

    Add files via upload

---
+++ Circular.java
@@ -0,0 +1,70 @@
+package com.intranet.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+@Entity
+@Table(name = "Circular")
+public class Circular {
+
+	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+	@Column(name = "CREATED_AT")
+	private DateTime createdAt;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "NAME")
+	private String name;
+
+	@Column(name = "FILE")
+	private byte[] pdfFile;
+
+	public DateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public byte[] getPdfFile() {
+		return pdfFile;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		final DateTime current = DateTime.now();
+		createdAt = current;
+	}
+
+	public void setCreatedAt(final DateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public void setPdfFile(final byte[] pdfFile) {
+		this.pdfFile = pdfFile;
+	}
+
+}
